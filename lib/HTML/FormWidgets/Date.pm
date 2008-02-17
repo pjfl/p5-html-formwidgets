8adf4f58fc7cbd033ecfb72f366a9d5636ebf55b
pjf <pjf@b09a3246-6927-4112-be6f-351db966f2cb>
2008-02-17 20:08:05 +0000

    Initial Release

---
+++ lib/HTML/FormWidgets/Date.pm
@@ -0,0 +1,23 @@
+package HTML::FormWidgets::Date;
+
+# @(#)$Id$
+
+use strict;
+use warnings;
+use base qw(HTML::FormWidgets);
+
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev$ =~ /\d+/gmx );
+
+sub _render {
+   my ($me, $ref) = @_; $ref->{size} = $me->width;
+
+   return $me->elem->textfield( $ref );
+}
+
+1;
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:
+

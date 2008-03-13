3561f39532df8a11073be751ec108647b3f8f05b
pjf <pjf@b09a3246-6927-4112-be6f-351db966f2cb>
2008-03-13 21:45:18 +0000

    Added images to distro

--- lib/HTML/FormWidgets/Freelist.pm
+++ lib/HTML/FormWidgets/Freelist.pm
@@ -9,58 +9,59 @@ use base qw(HTML::FormWidgets);
 use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev$ =~ /\d+/gmx );
 
 sub _render {
-   my ($me, $ref) = @_; my ($html, $rNo, $text, $text1, $tip, $val);
+   my ($me, $ref) = @_; my ($htag, $html, $rNo, $text, $text1, $tip, $val);
 
-   $ref->{name}     = 'new'.$me->name;
-   $ref->{size}     = $me->width || 20;
-   $html            = $me->elem->div({ class => 'container' },
-                                     $me->elem->textfield($ref));
-   $html           .= $me->elem->div({ class => 'separator' }, '&nbsp;');
+   $htag            = $me->elem;
+   $ref->{name    } = q(new).$me->name;
+   $ref->{size    } = $me->width || 20;
+   $html            = $htag->div( { class => q(container) },
+                                  $htag->textfield( $ref ) );
+   $html           .= $htag->div( { class => q(separator) }, q(&nbsp;) );
 
    $ref             = {};
-   $ref->{class}    = $ref->{name} = 'button';
-   $ref->{onclick}  = 'return freeListObj.AddItem(\''.$me->name.'\')';
-   $ref->{src}      = $me->assets.'AddItem.png';
-   $ref->{value}    = 'add'.(ucfirst $me->name);
-   $text            = $me->elem->image_button($ref);
+   $ref->{class   } = $ref->{name} = q(button);
+   $ref->{onclick } = 'return freeListObj.AddItem(\''.$me->name.'\')';
+   $ref->{src     } = $me->assets.'AddItem.png';
+   $ref->{value   } = q(add).(ucfirst $me->name);
+   $text            = $htag->image_button( $ref );
    $tip             = 'Enter a new item into the adjacent text field ';
    $tip            .= 'and then click this button to add it to the list';
-   $ref             = { class => 'help tips', title => $tip };
-   $text1           = $me->elem->span($ref, $text).$me->elem->br();
+   $ref             = { class => q(help tips), title => $tip };
+   $text1           = $htag->span( $ref, $text ).$htag->br();
 
    $ref             = {};
-   $ref->{class}    = $ref->{name} = 'button';
-   $ref->{onclick}  = 'return freeListObj.RemoveItem(\''.$me->name.'\')';
-   $ref->{src}      = $me->assets.'RemoveItem.png';
-   $ref->{value}    = 'remove'.(ucfirst $me->name);
-   $text            = $me->elem->image_button($ref);
+   $ref->{class   } = $ref->{name} = q(button);
+   $ref->{onclick } = 'return freeListObj.RemoveItem(\''.$me->name.'\')';
+   $ref->{src     } = $me->assets.'RemoveItem.png';
+   $ref->{value   } = q(remove).(ucfirst $me->name);
+   $text            = $htag->image_button( $ref );
    $tip             = 'Select one or more items from the adjacent list ';
    $tip            .= 'and then click this button to remove them';
-   $ref             = { class => 'help tips', title => $tip };
-   $text1          .= $me->elem->span($ref, $text);
-   $html           .= $me->elem->div({ class => 'container' }, $text1);
+   $ref             = { class => q(help tips), title => $tip };
+   $text1          .= $htag->span( $ref, $text );
+   $html           .= $htag->div( { class => q(container) }, $text1 );
 
    $ref             = {};
-   $ref->{labels}   = $me->labels if ($me->labels);
-   $ref->{multiple} = 'true';
-   $ref->{name}     = 'cur'.$me->name;
-   $ref->{size}     = $me->height;
-   $ref->{values}   = $me->values;
-   $html           .= $me->elem->scrolling_list($ref);
+   $ref->{labels  } = $me->labels if ($me->labels);
+   $ref->{multiple} = q(true);
+   $ref->{name    } = q(cur).$me->name;
+   $ref->{size    } = $me->height;
+   $ref->{values  } = $me->values;
+   $html           .= $htag->scrolling_list( $ref );
    $rNo             = 0;
 
-   for $val (@{$ref->{values}}) {
+   for $val (@{ $ref->{values} }) {
       $ref          = {};
-      $ref->{id}    = $me->name.$rNo++;
-      $ref->{name}  = $me->name;
+      $ref->{id   } = $me->name.$rNo++;
+      $ref->{name } = $me->name;
       $ref->{value} = $val;
-      $html        .= $me->elem->hidden($ref);
+      $html        .= $htag->hidden( $ref );
    }
 
    $ref             = {};
-   $ref->{name}     = 'nRows'.$me->name;
-   $ref->{value}    = $rNo;
-   $html           .= $me->elem->hidden($ref);
+   $ref->{name    } = q(nRows).$me->name;
+   $ref->{value   } = $rNo;
+   $html           .= $htag->hidden( $ref );
    return $html;
 }
 

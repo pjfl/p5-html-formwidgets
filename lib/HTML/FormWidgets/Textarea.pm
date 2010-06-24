aca82ede432e9754bb638f45d54d932c7c207e1f
pjf <pjf@b09a3246-6927-4112-be6f-351db966f2cb>
2010-06-24 23:41:32 +0000

    Set wrap to soft by default for text areas

--- lib/HTML/FormWidgets/Textarea.pm
+++ lib/HTML/FormWidgets/Textarea.pm
@@ -7,13 +7,14 @@ use warnings;
 use version; our $VERSION = qv( sprintf '0.6.%d', q$Rev$ =~ /\d+/gmx );
 use parent qw(HTML::FormWidgets);
 
-__PACKAGE__->mk_accessors( qw(height width) );
+__PACKAGE__->mk_accessors( qw(height width wrap) );
 
 sub init {
    my ($self, $args) = @_;
 
    $self->height( 5  );
    $self->width ( 60 );
+   $self->wrap  ( q(soft) );
    return;
 }
 
@@ -23,6 +24,7 @@ sub render_field {
    $args->{class} .= q( ifield).($self->class ? q( ).$self->class : q());
    $args->{cols }  = $self->width;
    $args->{rows }  = $self->height;
+   $args->{wrap }  = $self->wrap;
 
    return $self->hacc->textarea( $args );
 }

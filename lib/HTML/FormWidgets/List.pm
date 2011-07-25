725f8709cc063328429aed019f9649e8200493e9
pjf <pjf@b09a3246-6927-4112-be6f-351db966f2cb>
2011-07-25 03:01:10 +0000

    Added list subclass

---
+++ lib/HTML/FormWidgets/List.pm
@@ -0,0 +1,51 @@
+# @(#)$Id$
+
+package HTML::FormWidgets::UnorderedList;
+
+use strict;
+use warnings;
+use version; our $VERSION = qv( sprintf '0.7.%d', q$Rev$ =~ /\d+/gmx );
+use parent qw(HTML::FormWidgets);
+
+__PACKAGE__->mk_accessors( qw(config data item_class) );
+
+my $NBSP = '&#160;';
+
+sub init {
+   my ($self, $args) = @_;
+
+   $self->config    ( {} );
+   $self->class     ( q(plain) );
+   $self->data      ( [] );
+   $self->item_class( undef );
+   return;
+}
+
+sub render_field {
+   my ($self, $args) = @_; my $hacc = $self->hacc; my ($data, $html);
+
+   ($data = $self->data and $data->[ 0 ]) or return; my $js_args = {};
+
+   for (grep { exists $self->config->{ $_ } }
+        qw(direction duration nitems speed transition)) {
+      $js_args->{ $_ } = '"'.$self->config->{ $_ }.'"';
+   }
+
+   $self->_js_config( q(lists), $args->{id}, $js_args );
+
+   my $item_args = $self->item_class ? { class => $self->item_class } : {} ;
+
+   for my $item (@{ $data }) {
+      $html .= $hacc->li( $item_args,
+                          $self->inflate( $item->{content} || $NBSP ) );
+   }
+
+   return $hacc->ul( { class => $self->class, id => $args->{id} }, $html );
+}
+
+1;
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:

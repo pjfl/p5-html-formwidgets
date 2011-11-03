b99908ae44a7c53b4899f2c127090ff9084cf4d8
pjf <pjf@b09a3246-6927-4112-be6f-351db966f2cb>
2011-11-03 03:15:40 +0000

    Textarea simplified

--- lib/HTML/FormWidgets/Textarea.pm
+++ lib/HTML/FormWidgets/Textarea.pm
@@ -7,24 +7,10 @@ use warnings;
 use version; our $VERSION = qv( sprintf '0.7.%d', q$Rev$ =~ /\d+/gmx );
 use parent qw(HTML::FormWidgets);
 
-__PACKAGE__->mk_accessors( qw(height width wrap) );
-
-sub init {
-   my ($self, $args) = @_;
-
-   $self->height( 5  );
-   $self->width ( 60 );
-   $self->wrap  ( q(soft) );
-   return;
-}
-
 sub render_field {
    my ($self, $args)  = @_;
 
    $args->{class} .= q( ifield).($self->class ? q( ).$self->class : q());
-   $args->{cols }  = $self->width;
-   $args->{rows }  = $self->height;
-   $args->{wrap }  = $self->wrap;
 
    return $self->hacc->textarea( $args );
 }

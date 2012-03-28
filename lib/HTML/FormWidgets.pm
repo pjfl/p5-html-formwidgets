8361294ea93fd2a111b186e68d98224c324a27b9
pjf <pjf@b09a3246-6927-4112-be6f-351db966f2cb>
2012-03-28 23:41:55 +0000

    Funs over stopping CPAN testing Updated inc::Bob. Added inc::CPANTesting

--- lib/HTML/FormWidgets.pm
+++ lib/HTML/FormWidgets.pm
@@ -4,7 +4,7 @@ package HTML::FormWidgets;
 
 use strict;
 use warnings;
-use version; our $VERSION = qv( sprintf '0.11.%d', q$Rev$ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.12.%d', q$Rev$ =~ /\d+/gmx );
 use parent qw(Class::Accessor::Fast);
 
 use Class::MOP;
@@ -509,7 +509,7 @@ HTML::FormWidgets - Create HTML user interface components
 
 =head1 Version
 
-0.11.$Rev$
+0.12.$Rev$
 
 =head1 Synopsis
 
--- lib/HTML/FormWidgets/Cloud.pm
+++ lib/HTML/FormWidgets/Cloud.pm
@@ -4,7 +4,7 @@ package HTML::FormWidgets::Cloud;
 
 use strict;
 use warnings;
-use version; our $VERSION = qv( sprintf '0.11.%d', q$Rev$ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.12.%d', q$Rev$ =~ /\d+/gmx );
 use parent qw(HTML::FormWidgets);
 
 __PACKAGE__->mk_accessors( qw(data height) );

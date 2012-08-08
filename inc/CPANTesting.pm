e853c76cfe93c816d61bd07bebce1989c1b2fd03
pjf <pjf@b09a3246-6927-4112-be6f-351db966f2cb>
2012-08-08 15:49:01 +0000

    Updated inc::Bob to 1.4

--- inc/CPANTesting.pm
+++ inc/CPANTesting.pm
@@ -5,19 +5,28 @@ package CPANTesting;
 use strict;
 use warnings;
 
-my $uname = qx(uname -a);
+my $osname = lc $^O; my $uname = qx(uname -a);
 
-sub broken_toolchain {
+sub should_abort {
    return 0;
 }
 
-sub exceptions {
-   lc $^O eq q(cygwin)  and return 'Cygwin not supported';
-   lc $^O eq q(mirbsd)  and return 'Mirbsd not supported';
-   lc $^O eq q(mswin32) and return 'Mswin  not supported';
-   lc $^O eq q(netbsd)  and return 'Netbsd not supported';
+sub test_exceptions {
+   my $p = shift; __is_testing() or return 0;
+
+   $p->{stop_tests} and return 'CPAN Testing stopped in Build.PL';
+
+   $osname eq q(cygwin)  and return 'Cygwin OS unsupported';
+   $osname eq q(mirbsd)  and return 'Mirbsd OS unsupported';
+   $osname eq q(mswin32) and return 'Mswin  OS unsupported';
+   $osname eq q(netbsd)  and return 'Netbsd OS unsupported';
    return 0;
 }
+
+# Private functions
+
+sub __is_testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
+                   || ($ENV{PERL5OPT} || q()) =~ m{ CPAN-Reporter }mx) }
 
 1;
 
